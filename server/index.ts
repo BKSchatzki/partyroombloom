812df2efec71e77dd498a9f43c59a471e71ed5b5
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2024-03-28 23:40:54 -0700

    Configure server script and install tsx to watch changes

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,13 @@ const prisma = new PrismaClient();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 9876;
+
 app.get('/api/users', async (req, res) => {
   const users = await prisma.user.findMany();
   res.json(users);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
