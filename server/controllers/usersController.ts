e0a7f114266097eb4bd9f547b2badafb7f18ea6c
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2024-03-29 01:53:41 -0700

    Simplify schema, set up test user routes and move .env and example to project root

---
+++ server/controllers/usersController.ts
@@ -0,0 +1,69 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const usersController = {
+  getUsers: async (req, res) => {
+    try {
+      const users = await prisma.user.findMany();
+      res.json(users);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error fetching users' });
+    }
+  },
+  getUserById: async (req, res) => {
+    const userId = req.params.id;
+    try {
+      const user = await prisma.user.findUnique({ where: { id: userId } });
+      res.json(user);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error fetching user' });
+    }
+  },
+  createUser: async (req, res) => {
+    try {
+      const { email, name } = req.body; // Destructure data from request body
+      const newUser = await prisma.user.create({
+        data: {
+          email,
+          name,
+        },
+      });
+      res.json(newUser);
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ message: 'Error creating user' }); // Handle specific errors like duplicate email
+    }
+  },
+  updateUser: async (req, res) => {
+    const userId = req.params.id;
+    const { email, name } = req.body;
+    try {
+      const updatedUser = await prisma.user.update({
+        where: { id: userId },
+        data: {
+          email,
+          name,
+        },
+      });
+      res.json(updatedUser);
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ message: 'Error updating user' }); // Handle specific errors like duplicate email
+    }
+  },
+  deleteUser: async (req, res) => {
+    const userId = req.params.id;
+    try {
+      await prisma.user.delete({ where: { id: userId } });
+      res.json({ message: 'User deleted successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error deleting user' });
+    }
+  },
+};
+
+export default usersController;
--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,15 @@
 import cors from 'cors';
 import express from 'express';
 
-import { PrismaClient } from '@prisma/client';
+import usersRouter from './routers/usersRouter';
 
 const app = express();
-const prisma = new PrismaClient();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 9876;
+app.use('/api/users', usersRouter);
 
-app.get('/api/users', async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-});
+const PORT = process.env.PORT || 9876;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
