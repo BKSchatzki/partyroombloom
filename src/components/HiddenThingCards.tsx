a04110ce2f1de430be344cd85ffaae3a08b415ac
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2024-04-03 05:32:25 -0700

    Reintroduce Masonry

--- src/components/HiddenThingCards.tsx
+++ src/components/HiddenThingCards.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Add, Clear } from '@mui/icons-material';
+import { Masonry } from '@mui/lab';
 import {
   Button,
   ButtonGroup,
@@ -10,7 +11,6 @@ import {
   Checkbox,
   Divider,
   FormControlLabel,
-  Grid,
   Stack,
   TextField,
 } from '@mui/material';
@@ -39,130 +39,138 @@ const HiddenThingCards = ({
   };
 
   return (
-    <Grid container>
+    <Masonry
+      columns={{ xs: 1, sm: 2, md: 3 }}
+      sequential
+    >
+      {/* <Grid container> */}
       {scene.landmarkThings.map((landmarkItem, landmarkIndex) => (
-        <Grid
+        // <Grid
+        //   key={landmarkIndex}
+        //   item
+        //   xs={12}
+        //   sm={6}
+        //   md={4}
+        // >
+        <Card
           key={landmarkIndex}
-          item
-          xs={12}
-          sm={6}
-          md={4}
+          sx={{ mx: 1, my: 1 }}
         >
-          <Card sx={{ mx: 1, my: 1 }}>
-            <CardHeader
-              title={
-                landmarkItem.landmarkName
-                  ? `📍 ${landmarkItem.landmarkName}`
-                  : `📍 Landmark Thing ${landmarkIndex + 1}`
-              }
-              titleTypographyProps={{
-                sx: { fontSize: '1.2rem' },
-              }}
-            />
-            <Divider />
-            {landmarkItem.hiddenThings.map((hiddenItem, hiddenIndex) => (
-              <CardContent key={hiddenIndex}>
-                <Stack spacing={2}>
-                  <TextField
-                    label={`🔎 Hidden Thing ${hiddenIndex + 1}`}
-                    value={hiddenItem.hiddenName}
-                    onChange={(e) => {
+          <CardHeader
+            title={
+              landmarkItem.landmarkName
+                ? `📍 ${landmarkItem.landmarkName}`
+                : `📍 Landmark Thing ${landmarkIndex + 1}`
+            }
+            titleTypographyProps={{
+              sx: { fontSize: '1.2rem' },
+            }}
+          />
+          <Divider />
+          {landmarkItem.hiddenThings.map((hiddenItem, hiddenIndex) => (
+            <CardContent key={hiddenIndex}>
+              <Stack spacing={2}>
+                <TextField
+                  label={`🔎 Hidden Thing ${hiddenIndex + 1}`}
+                  value={hiddenItem.hiddenName}
+                  onChange={(e) => {
+                    const updatedLandmarkItems = [...scene.landmarkThings];
+                    updatedLandmarkItems[landmarkIndex].hiddenThings[hiddenIndex].hiddenName =
+                      e.target.value;
+                    setScene({
+                      ...scene,
+                      landmarkThings: updatedLandmarkItems,
+                    });
+                  }}
+                  fullWidth={true}
+                  variant={'outlined'}
+                />
+                <TextField
+                  label={'Description'}
+                  value={hiddenItem.hiddenDescription}
+                  onChange={(e) => {
+                    const updatedLandmarkItems = [...scene.landmarkThings];
+                    updatedLandmarkItems[landmarkIndex].hiddenThings[
+                      hiddenIndex
+                    ].hiddenDescription = e.target.value;
+                    setScene({
+                      ...scene,
+                      landmarkThings: updatedLandmarkItems,
+                    });
+                  }}
+                  fullWidth={true}
+                  variant={'outlined'}
+                  multiline
+                  minRows={2}
+                  maxRows={Infinity}
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      color={'warning'}
+                      checked={hiddenItem.hasSecret}
+                      onChange={(e) => {
+                        const updatedLandmarkItems = [...scene.landmarkThings];
+                        updatedLandmarkItems[landmarkIndex].hiddenThings[hiddenIndex].hasSecret =
+                          e.target.checked;
+                        setScene({
+                          ...scene,
+                          landmarkThings: updatedLandmarkItems,
+                        });
+                      }}
+                    />
+                  }
+                  label={'🎲 Can roll for secrets'}
+                />
+                <ButtonGroup
+                  size={'small'}
+                  color={'inherit'}
+                  variant={'outlined'}
+                  fullWidth={true}
+                >
+                  <Button
+                    color={'error'}
+                    disabled={landmarkItem.hiddenThings.length === 1}
+                    onClick={() => {
                       const updatedLandmarkItems = [...scene.landmarkThings];
-                      updatedLandmarkItems[landmarkIndex].hiddenThings[hiddenIndex].hiddenName =
-                        e.target.value;
+                      updatedLandmarkItems[landmarkIndex].hiddenThings = updatedLandmarkItems[
+                        landmarkIndex
+                      ].hiddenThings.filter((_, i) => i !== hiddenIndex);
                       setScene({
                         ...scene,
                         landmarkThings: updatedLandmarkItems,
                       });
                     }}
-                    fullWidth={true}
-                    variant={'outlined'}
-                  />
-                  <TextField
-                    label={'Description'}
-                    value={hiddenItem.hiddenDescription}
-                    onChange={(e) => {
-                      const updatedLandmarkItems = [...scene.landmarkThings];
-                      updatedLandmarkItems[landmarkIndex].hiddenThings[
-                        hiddenIndex
-                      ].hiddenDescription = e.target.value;
-                      setScene({
-                        ...scene,
-                        landmarkThings: updatedLandmarkItems,
-                      });
-                    }}
-                    fullWidth={true}
-                    variant={'outlined'}
-                    multiline
-                    minRows={2}
-                    maxRows={Infinity}
-                  />
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        color={'warning'}
-                        checked={hiddenItem.hasSecret}
-                        onChange={(e) => {
-                          const updatedLandmarkItems = [...scene.landmarkThings];
-                          updatedLandmarkItems[landmarkIndex].hiddenThings[hiddenIndex].hasSecret =
-                            e.target.checked;
-                          setScene({
-                            ...scene,
-                            landmarkThings: updatedLandmarkItems,
-                          });
-                        }}
-                      />
-                    }
-                    label={'🎲 Can roll for secrets'}
-                  />
-                  <ButtonGroup
-                    size={'small'}
-                    color={'inherit'}
-                    variant={'outlined'}
-                    fullWidth={true}
                   >
-                    <Button
-                      color={'error'}
-                      disabled={landmarkItem.hiddenThings.length === 1}
-                      onClick={() => {
-                        const updatedLandmarkItems = [...scene.landmarkThings];
-                        updatedLandmarkItems[landmarkIndex].hiddenThings = updatedLandmarkItems[
-                          landmarkIndex
-                        ].hiddenThings.filter((_, i) => i !== hiddenIndex);
-                        setScene({
-                          ...scene,
-                          landmarkThings: updatedLandmarkItems,
-                        });
-                      }}
-                    >
-                      <Clear />
-                    </Button>
-                    <Button
-                      color={'primary'}
-                      sx={{ width: '500%' }}
-                      onClick={() => {
-                        const updatedLandmarkItems = [...scene.landmarkThings];
-                        updatedLandmarkItems[landmarkIndex].hiddenThings.splice(
-                          hiddenIndex + 1,
-                          0,
-                          newHiddenThing
-                        );
-                        setScene({
-                          ...scene,
-                          landmarkThings: updatedLandmarkItems,
-                        });
-                      }}
-                    >
-                      <Add />
-                    </Button>
-                  </ButtonGroup>
-                </Stack>{' '}
-              </CardContent>
-            ))}
-          </Card>
-        </Grid>
+                    <Clear />
+                  </Button>
+                  <Button
+                    color={'primary'}
+                    sx={{ width: '500%' }}
+                    onClick={() => {
+                      const updatedLandmarkItems = [...scene.landmarkThings];
+                      updatedLandmarkItems[landmarkIndex].hiddenThings.splice(
+                        hiddenIndex + 1,
+                        0,
+                        newHiddenThing
+                      );
+                      setScene({
+                        ...scene,
+                        landmarkThings: updatedLandmarkItems,
+                      });
+                    }}
+                  >
+                    <Add />
+                  </Button>
+                </ButtonGroup>
+              </Stack>{' '}
+            </CardContent>
+          ))}
+        </Card>
+        // </Grid>
       ))}
-    </Grid>
+      {/* </Grid> */}
+    </Masonry>
   );
 };
 
--- src/components/LandmarkThingCards.tsx
+++ src/components/LandmarkThingCards.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import { Add, Clear } from '@mui/icons-material';
-import { Button, ButtonGroup, Card, CardContent, Grid, Stack, TextField } from '@mui/material';
+import { Masonry } from '@mui/lab';
+import { Button, ButtonGroup, Card, CardContent, Stack, TextField } from '@mui/material';
 
 import type { Scene } from '../pages/SceneForm';
 
@@ -33,92 +34,100 @@ const LandmarkThingCards = ({
   };
 
   return (
-    <Grid container>
+    <Masonry
+      columns={{ xs: 1, sm: 2, md: 3 }}
+      sequential
+    >
+      {/* <Grid container> */}
       {scene.landmarkThings.map((landmarkItem, landmarkIndex) => (
-        <Grid
+        // <Grid
+        //   key={landmarkIndex}
+        //   item
+        //   xs={12}
+        //   sm={6}
+        //   md={4}
+        // >
+        <Card
           key={landmarkIndex}
-          item
-          xs={12}
-          sm={6}
-          md={4}
+          sx={{ mx: 1, my: 1 }}
         >
-          <Card sx={{ mx: 1, my: 1 }}>
-            <CardContent>
-              <Stack spacing={2}>
-                <TextField
-                  label={`📍 Landmark Thing ${landmarkIndex + 1}`}
-                  value={landmarkItem.landmarkName}
-                  onChange={(e) => {
-                    const updatedItems = [...scene.landmarkThings];
-                    updatedItems[landmarkIndex].landmarkName = e.target.value;
+          <CardContent>
+            <Stack spacing={2}>
+              <TextField
+                label={`📍 Landmark Thing ${landmarkIndex + 1}`}
+                value={landmarkItem.landmarkName}
+                onChange={(e) => {
+                  const updatedItems = [...scene.landmarkThings];
+                  updatedItems[landmarkIndex].landmarkName = e.target.value;
+                  setScene({
+                    ...scene,
+                    landmarkThings: updatedItems,
+                  });
+                }}
+                fullWidth={true}
+                variant={'outlined'}
+              />
+              <TextField
+                label={'Description'}
+                value={landmarkItem.landmarkDescription}
+                onChange={(e) => {
+                  const updatedItems = [...scene.landmarkThings];
+                  updatedItems[landmarkIndex].landmarkDescription = e.target.value;
+                  setScene({
+                    ...scene,
+                    landmarkThings: updatedItems,
+                  });
+                }}
+                fullWidth={true}
+                variant={'outlined'}
+                multiline
+                minRows={2}
+                maxRows={Infinity}
+              />
+              <ButtonGroup
+                size={'small'}
+                color={'inherit'}
+                variant={'outlined'}
+                fullWidth={true}
+              >
+                <Button
+                  color={'error'}
+                  disabled={scene.landmarkThings.length === 1}
+                  onClick={() => {
+                    const updatedItems = [
+                      ...scene.landmarkThings.filter((_, i) => i !== landmarkIndex),
+                    ];
                     setScene({
                       ...scene,
                       landmarkThings: updatedItems,
                     });
                   }}
-                  fullWidth={true}
-                  variant={'outlined'}
-                />
-                <TextField
-                  label={'Description'}
-                  value={landmarkItem.landmarkDescription}
-                  onChange={(e) => {
-                    const updatedItems = [...scene.landmarkThings];
-                    updatedItems[landmarkIndex].landmarkDescription = e.target.value;
-                    setScene({
-                      ...scene,
-                      landmarkThings: updatedItems,
-                    });
-                  }}
-                  fullWidth={true}
-                  variant={'outlined'}
-                  multiline
-                  minRows={2}
-                  maxRows={Infinity}
-                />
-                <ButtonGroup
-                  size={'small'}
-                  color={'inherit'}
-                  variant={'outlined'}
-                  fullWidth={true}
                 >
-                  <Button
-                    color={'error'}
-                    disabled={scene.landmarkThings.length === 1}
-                    onClick={() => {
-                      const updatedItems = [
-                        ...scene.landmarkThings.filter((_, i) => i !== landmarkIndex),
-                      ];
-                      setScene({
-                        ...scene,
-                        landmarkThings: updatedItems,
-                      });
-                    }}
-                  >
-                    <Clear />
-                  </Button>
-                  <Button
-                    color={'primary'}
-                    sx={{ width: '500%' }}
-                    onClick={() => {
-                      const updatedItems = [...scene.landmarkThings];
-                      const itemIndex = landmarkIndex;
-                      updatedItems.splice(itemIndex + 1, 0, newLandmarkThing);
-                      setScene({
-                        ...scene,
-                        landmarkThings: updatedItems,
-                      });
-                    }}
-                  >
-                    <Add />
-                  </Button>
-                </ButtonGroup>
-              </Stack>{' '}
-            </CardContent>
-          </Card>
-        </Grid>
+                  <Clear />
+                </Button>
+                <Button
+                  color={'primary'}
+                  sx={{ width: '500%' }}
+                  onClick={() => {
+                    const updatedItems = [...scene.landmarkThings];
+                    const itemIndex = landmarkIndex;
+                    updatedItems.splice(itemIndex + 1, 0, newLandmarkThing);
+                    setScene({
+                      ...scene,
+                      landmarkThings: updatedItems,
+                    });
+                  }}
+                >
+                  <Add />
+                </Button>
+              </ButtonGroup>
+            </Stack>{' '}
+          </CardContent>
+        </Card>
+        // </Grid>
       ))}
-    </Grid>
+      {/* </Grid> */}
+    </Masonry>
   );
 };
 
