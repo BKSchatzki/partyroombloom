e8a5c7716cca4da1a1725921a559fbc90a38207e
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2024-11-10 20:47:00 -0800

    Fix issue with POST causing child elements to potentially be inserted before their parents

--- src/app/api/outline/route.ts
+++ src/app/api/outline/route.ts
@@ -65,23 +65,31 @@ export const POST = async (req: NextRequest) => {
         comments: outline.comments ?? '',
       },
     });
-    const elementPromises = outline.elements.map((element: Element) =>
-      prisma.element.create({
-        data: {
-          id: element.id,
-          outlineId: createdOutline.id,
-          userId: user.id,
-          parentId: element.parentId,
-          type: element.type,
-          name: element.name ?? '',
-          description: element.description ?? '',
-          rollableSuccess: element.rollableSuccess ?? '',
-          rollableFailure: element.rollableFailure ?? '',
-          userCreatedAt: element.userCreatedAt ? new Date(element.userCreatedAt) : undefined,
-        },
-      })
-    );
-    await Promise.all(elementPromises);
+    const landmarks = outline.elements.filter((element) => element.type === 'landmark');
+    const interactables = outline.elements.filter((element) => element.type === 'interactable');
+    const secrets = outline.elements.filter((element) => element.type === 'secret');
+    const createElements = async (elements: Element[]) => {
+      const elementPromises = elements.map((element: Element) =>
+        prisma.element.create({
+          data: {
+            id: element.id,
+            outlineId: createdOutline.id,
+            userId: user.id,
+            parentId: element.parentId,
+            type: element.type,
+            name: element.name ?? '',
+            description: element.description ?? '',
+            rollableSuccess: element.rollableSuccess ?? '',
+            rollableFailure: element.rollableFailure ?? '',
+            userCreatedAt: element.userCreatedAt ? new Date(element.userCreatedAt) : undefined,
+          },
+        })
+      );
+      return Promise.all(elementPromises);
+    };
+    await createElements(landmarks);
+    await createElements(interactables);
+    await createElements(secrets);
     return Response.json({ id: createdOutline.id }, { status: 200 });
   } catch (error) {
     console.error('Error saving outline:', error);
