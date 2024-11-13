a8ae077030acdbd36706ea90cb635cdd12197950
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2024-11-12 20:30:47 -0800

    Compact loading states into main return of OutlinesList

--- src/app/overview/_components/OutlinesList.tsx
+++ src/app/overview/_components/OutlinesList.tsx
@@ -60,48 +60,8 @@ const OutlinesList = () => {
     }
   };
 
-  if (isLoading) {
-    return (
-      <div className={cn(`flex h-full w-full flex-col gap-4 p-4`)}>
-        <Skeleton className={cn(`flex h-80 w-full flex-col items-center justify-center`)}>
-          <span className="loading loading-spinner loading-lg"></span>
-        </Skeleton>
-        <Skeleton className={cn(`flex h-80 w-full flex-col items-center justify-center`)}>
-          <span className="loading loading-spinner loading-lg"></span>
-        </Skeleton>
-        <Skeleton className={cn(`flex h-80 w-full flex-col items-center justify-center`)}>
-          <span className="loading loading-spinner loading-lg"></span>
-        </Skeleton>
-      </div>
-    );
-  }
-
   if (error) {
     return <div>Error: {JSON.stringify(error)}</div>;
-  }
-
-  if (outlinesList.length === 0) {
-    return (
-      <div className={cn(`flex h-full w-full flex-col items-center justify-center p-4`)}>
-        <Card
-          className={cn(
-            `bg-base-200 flex max-w-80 text-balance border-none text-center transition-all duration-100 ease-in-out hover:brightness-[115%]`
-          )}
-        >
-          <Link
-            href={`/outline`}
-            className={cn(`flex flex-col items-center justify-center gap-2 p-6`)}
-          >
-            <h1 className={cn(`text-3xl`)}>No Outlines Found</h1>
-            <Plus
-              size={160}
-              className={cn(`text-4xl`)}
-            />
-            <p className={cn(`text-xl`)}>Create a new Outline to get started.</p>
-          </Link>
-        </Card>
-      </div>
-    );
   }
 
   return (
@@ -119,89 +79,95 @@ const OutlinesList = () => {
           New Outline
         </Card>
       </Link>
-      <Accordion
-        type={`single`}
-        collapsible={true}
-        className={cn(`border-t-2 border-base-300 pt-4`)}
-      >
-        {outlinesList.map((outline) => (
-          <Card
-            key={outline.id}
-            className={cn(
-              `relative mb-6 w-full bg-neutral/50 p-0 shadow-xl shadow-base-300 transition-all duration-100 ease-in-out max-sm:rounded-none`
-            )}
-          >
-            <AccordionItem
+      {isLoading ? (
+        <Skeleton className={cn(`flex h-screen w-full flex-col items-center p-8`)}>
+          <span className="loading loading-spinner loading-lg"></span>
+        </Skeleton>
+      ) : (
+        <Accordion
+          type={`single`}
+          collapsible={true}
+          className={cn(`border-t-2 border-base-300 pt-4`)}
+        >
+          {outlinesList.map((outline) => (
+            <Card
               key={outline.id}
-              value={`${outline.id}`}
+              className={cn(
+                `relative mb-6 w-full bg-neutral/50 p-0 shadow-xl shadow-base-300 transition-all duration-100 ease-in-out max-sm:rounded-none`
+              )}
             >
-              <AccordionTrigger iconSize={9}>
-                <span
-                  className={cn(
-                    `flex w-full shrink-0 gap-2 text-balance p-4 text-3xl text-[#64d8b4] hover:brightness-125 max-sm:flex-col sm:basis-1/3 sm:gap-4`,
-                    !outline.title && `italic opacity-30`
-                  )}
-                >
-                  {outline.title || 'Untitled Scene'}
-                </span>
-              </AccordionTrigger>
-              <AccordionContent>
-                <div className={cn(`flex flex-col gap-2 border-t-2 border-base-300 p-4`)}>
-                  <span className={cn(!outline.description && `italic opacity-30`)}>
-                    <span className={cn(`font-semibold text-[#64d8b4]`)}>Description: </span>
-                    {outline.description || 'A short description to set the scene.'}
+              <AccordionItem
+                key={outline.id}
+                value={`${outline.id}`}
+              >
+                <AccordionTrigger iconSize={9}>
+                  <span
+                    className={cn(
+                      `flex w-full shrink-0 gap-2 text-balance p-4 text-3xl text-[#64d8b4] hover:brightness-125 max-sm:flex-col sm:basis-1/3 sm:gap-4`,
+                      !outline.title && `italic opacity-30`
+                    )}
+                  >
+                    {outline.title || 'Untitled Scene'}
                   </span>
-                  <span className={cn(!outline.goal && `italic opacity-30`)}>
-                    <span className={cn(`font-semibold text-[#64d8b4]`)}>Goal: </span>
-                    {outline.goal || 'What this scene does or where it leads.'}
-                  </span>
-                  <span className={cn(!outline.comments && `italic opacity-30`)}>
-                    <span className={cn(`font-semibold text-[#64d8b4]`)}>Comments: </span>
-                    {outline.comments || 'Extra information to help the Simulator or Future You.'}
-                  </span>
-                </div>
-                <div className={cn(`grid grid-cols-12 gap-4 border-t-2 border-base-300/30 p-4`)}>
-                  <div className={cn(`col-span-12 sm:col-span-4`)}>
-                    <DeleteButton
-                      block={true}
-                      handleDelete={() => handleDelete(outline.id as number)}
-                      item={outline.title || 'this Outline'}
-                      message={`Delete Outline`}
-                    />
+                </AccordionTrigger>
+                <AccordionContent>
+                  <div className={cn(`flex flex-col gap-2 border-t-2 border-base-300 p-4`)}>
+                    <span className={cn(!outline.description && `italic opacity-30`)}>
+                      <span className={cn(`font-semibold text-[#64d8b4]`)}>Description: </span>
+                      {outline.description || 'A short description to set the scene.'}
+                    </span>
+                    <span className={cn(!outline.goal && `italic opacity-30`)}>
+                      <span className={cn(`font-semibold text-[#64d8b4]`)}>Goal: </span>
+                      {outline.goal || 'What this scene does or where it leads.'}
+                    </span>
+                    <span className={cn(!outline.comments && `italic opacity-30`)}>
+                      <span className={cn(`font-semibold text-[#64d8b4]`)}>Comments: </span>
+                      {outline.comments || 'Extra information to help the Simulator or Future You.'}
+                    </span>
                   </div>
-                  <Link
-                    href={`/outline/${outline.id}`}
-                    className={cn(`col-span-12 sm:col-span-4`)}
-                  >
-                    <Button
-                      color={`secondary`}
-                      size={`block`}
-                      className={cn(`max-w-full`)}
+                  <div className={cn(`grid grid-cols-12 gap-4 border-t-2 border-base-300/30 p-4`)}>
+                    <div className={cn(`col-span-12 sm:col-span-4`)}>
+                      <DeleteButton
+                        block={true}
+                        handleDelete={() => handleDelete(outline.id as number)}
+                        item={outline.title || 'this Outline'}
+                        message={`Delete Outline`}
+                      />
+                    </div>
+                    <Link
+                      href={`/outline/${outline.id}`}
+                      className={cn(`col-span-12 sm:col-span-4`)}
                     >
-                      <Pencil className={cn(`size-5`)} />
-                      Edit Outline
-                    </Button>
-                  </Link>
-                  <Link
-                    href={`/outline/${outline.id}`}
-                    className={cn(`col-span-12 sm:col-span-4`)}
-                  >
-                    <Button
-                      color={`primary`}
-                      size={`block`}
-                      className={cn(`max-w-full`)}
+                      <Button
+                        color={`secondary`}
+                        size={`block`}
+                        className={cn(`max-w-full`)}
+                      >
+                        <Pencil className={cn(`size-5`)} />
+                        Edit Outline
+                      </Button>
+                    </Link>
+                    <Link
+                      href={`/outline/${outline.id}`}
+                      className={cn(`col-span-12 sm:col-span-4`)}
                     >
-                      <Sparkle className={cn(`size-5`)} />
-                      Simulate Scene
-                    </Button>
-                  </Link>
-                </div>
-                <Preview outline={outline} />
-              </AccordionContent>
-            </AccordionItem>
-          </Card>
-        ))}
-      </Accordion>
+                      <Button
+                        color={`primary`}
+                        size={`block`}
+                        className={cn(`max-w-full`)}
+                      >
+                        <Sparkle className={cn(`size-5`)} />
+                        Simulate Scene
+                      </Button>
+                    </Link>
+                  </div>
+                  <Preview outline={outline} />
+                </AccordionContent>
+              </AccordionItem>
+            </Card>
+          ))}
+        </Accordion>
+      )}
     </ScrollArea>
   );
 };
